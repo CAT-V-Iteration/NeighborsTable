aaf2aebe44d8146147f525508fc36beb8f17cda3
Alexander Infante <alexinfante17@gmail.com>
2020-04-22 16:30:37 -0700

    user.js and userController.js outline and structure completed, middleware is still empty

--- src/server/controllers/userControllers.js
+++ src/server/controllers/userControllers.js
@@ -1,5 +1,54 @@
 const db = require('../models/models.js');
 
+// userControllers and methods ------------------------
 const userControllers = {};
 
+/* -------------------------- TEST -------------------------- */
+
+// GET /test ---------------------------------------------------
+// Get all user data
+userControllers.getAll = (req, res, next) => {
+
+};
+
+
+/* ---------------------- SIGNUP/LOGIN ---------------------- */
+
+// GET /login ------------------------------------------------
+// Check user info against databse
+userControllers.verify = (req, res, next) => {
+  
+};
+
+
+// POST /login -----------------------------------------------
+// Create new user data
+userControllers.signUp = (req, res, next) => {
+
+};
+
+
+/* ------------------- USER/SELLER INFO ------------------- */
+
+// PUT /user
+// Register user as seller
+userControllers.registerSeller = (req, res, next) => {
+  
+};
+
+
+// GET /user
+// Get seller_id from user data and attach to new product
+userControllers.sellerID = (req, res, next) => {
+  
+};
+
+
+/* ----------------------- STRETCH ----------------------- */
+// PUT /login ------------------------------------------------
+// Change password
+
+
+
+// Exports --------------------------------------------
 module.exports = userControllers;
--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -2,5 +2,62 @@ const express = require('express');
 const userControllers = require('../controllers/userControllers')
 const router = express.Router();
 
+/* -------------------------- TEST -------------------------- */
+
+// GET /test ---------------------------------------------------
+// Get all user data
+router.get('/test',
+  //middeware
+  userControllers.getAll,
+  (req, res) => res.status(200).json(res.locals.users)
+);
+
+
+/* ---------------------- SIGNUP/LOGIN ---------------------- */
+
+// GET /login ------------------------------------------------
+// Check user info against databse
+router.get('/login',
+  //middeware
+  userControllers.verify,
+  (req, res) => res.status(200).json(/* res.locals. */)
+);
+
+
+// POST /login -----------------------------------------------
+// Create new user data
+router.post('/login',
+  //middeware
+  userControllers.signUp,
+  (req, res) => res.status(200).json(/* res.locals. */)
+);
+
+/* ------------------- USER/SELLER INFO ------------------- */
+
+// PUT /user
+// Register user as seller
+router.put('/user',
+  //middeware
+  userControllers.registerSeller,
+  (req, res) => res.status(200).json(/* res.locals. */)
+);
+
+// GET /user
+// Get seller_id from user data and attach to new product
+router.get('/user',
+  //middeware
+  userControllers.sellerID,
+  (req, res) => res.status(200).json(/* res.locals. */)
+);
+
+
+/* ----------------------- STRETCH ----------------------- */
+// PUT /login ------------------------------------------------
+// Change password
+// router.put('/login',
+//   //middeware
+//   (req, res) => res.status(200).json(/* res.locals. */)
+// );
+
 
 module.exports = router;
